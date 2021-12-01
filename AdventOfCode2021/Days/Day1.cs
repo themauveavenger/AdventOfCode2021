2491d38ab51b8873693012855bd77c19f07da49a
Josh P <josh.petersen@protonmail.com>
2021-12-01 16:37:32 -0500

    initial commit - day 1

---
+++ AdventOfCode2021/Days/Day1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2021.Days
+{
+    public class Day1
+    {
+        string inputPath = "C:\\Users\\jpete\\Documents\\projects\\AdventOfCode2021\\AdventOfCode2021\\Days\\Day1Resources\\input.txt";
+        public string[] ReadFileLines(string path)
+        {
+            return System.IO.File.ReadAllLines(path);
+        }
+        public void Run()
+        {
+            var input = ReadFileLines(inputPath);
+            var depthIncreasedCount = 0;
+            for (int i = 1; i < input.Length; i += 1)
+            {
+                var depth = Convert.ToInt32(input[i]);
+                var prevDepth = Convert.ToInt32(input[i - 1]);
+
+                if (depth > prevDepth)
+                {
+                    depthIncreasedCount += 1;
+                }
+            }
+
+            Console.WriteLine($"depth increased count: {depthIncreasedCount}");
+        }
+
+        public void RunPart2()
+        {
+            var input = ReadFileLines(inputPath).Select(i => Convert.ToInt32(i)).ToArray();
+            var sumIncreasedCount = 0;
+            var prevSum = 0;
+            for (int i = 2; i < input.Length; i += 1)
+            {
+                // get the window, which is the previous 2 entries
+                var a = input[i - 2];
+                var b = input[i - 1];
+                var c = input[i];
+
+                var sum = a + b + c;
+                if (prevSum != 0 && sum > prevSum)
+                {
+                    sumIncreasedCount += 1;
+                }
+                prevSum = sum;
+            }
+
+            Console.WriteLine($"part 2 sum increased count: {sumIncreasedCount}");
+        }
+    }
+}
---
+++ AdventOfCode2021/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using AdventOfCode2021.Days;
+
+namespace AdventOfCode2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var d1 = new Day1();
+            d1.Run();
+            d1.RunPart2();
+        }
+    }
+}
