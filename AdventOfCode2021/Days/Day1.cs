3d14d09daa72c872f31fdb8945dc404c17a40175
Josh P <josh.petersen@protonmail.com>
2021-12-01 21:26:39 -0500

    smoe cleanup

--- AdventOfCode2021/Days/Day1.cs
+++ AdventOfCode2021/Days/Day1.cs
@@ -10,18 +10,20 @@ namespace AdventOfCode2021.Days
     public class Day1
     {
         string inputPath = "C:\\Users\\jpete\\Documents\\projects\\AdventOfCode2021\\AdventOfCode2021\\Days\\Day1Resources\\input.txt";
+
         public string[] ReadFileLines(string path)
         {
             return System.IO.File.ReadAllLines(path);
         }
+
         public void Run()
         {
-            var input = ReadFileLines(inputPath);
+            var input = ReadFileLines(inputPath).Select(i => Convert.ToInt32(i)).ToArray();
             var depthIncreasedCount = 0;
             for (int i = 1; i < input.Length; i += 1)
             {
-                var depth = Convert.ToInt32(input[i]);
-                var prevDepth = Convert.ToInt32(input[i - 1]);
+                var depth = input[i];
+                var prevDepth = input[i - 1];
 
                 if (depth > prevDepth)
                 {
@@ -39,7 +41,7 @@ namespace AdventOfCode2021.Days
             var prevSum = 0;
             for (int i = 2; i < input.Length; i += 1)
             {
-                // get the window, which is the previous 2 entries
+                // get the window, which is current & the previous 2 entries
                 var a = input[i - 2];
                 var b = input[i - 1];
                 var c = input[i];
